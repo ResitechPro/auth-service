5090810ece42e18f979797a278bc046d52807cf4
MohamedJamh <jam7.mohamed@gmail.com>
2024-01-14 00:28:30 +0100

    RESPRO-13 - add : multi tenancy configuration

---
+++ src/main/java/com/taskflow/config/resolver/JwtTenantResolver.java
@@ -0,0 +1,27 @@
+package com.taskflow.config.resolver;
+
+import com.taskflow.service.JwtService;
+import com.taskflow.utils.OrganizationNameExtractor;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtTenantResolver implements TenantResolver<HttpServletRequest> {
+    private final JwtService jwtService;
+    private final OrganizationNameExtractor organizationNameExtractor;
+    public JwtTenantResolver(JwtService jwtService, OrganizationNameExtractor organizationNameExtractor) {
+        this.jwtService = jwtService;
+        this.organizationNameExtractor = organizationNameExtractor;
+    }
+
+    @Override
+    public String resolveTenant(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        if(authHeader != null && authHeader.startsWith("Bearer ")){
+            String jwtToken = authHeader.substring(7);
+            String email = jwtService.extractUserName(jwtToken);
+            return organizationNameExtractor.extract(email);
+        }
+        return "public";
+    }
+}
